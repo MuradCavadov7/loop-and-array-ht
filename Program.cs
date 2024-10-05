5531041e33ba2c5359b803837ef5b893b788a31c
MuradCavadov7 <muradnc-ap117@code.edu.az>
2024-10-06 00:12:41 +0400

    loop and array

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿#region 
+// 3.Ədədlərdən ibarət arrayda neçə rəqəm olduğunu ekrana çap edin (Məs: {1,77,6,14} arrayında 2 rəqəm var 
+
+//int[] numbers = { 3, 100, 23, 45, 1, 2, 55, 6,9, 78 };
+//int reqemSayi = 0;
+//for (int i = 0; numbers.Length > i; i++)
+//{
+//    if (numbers[i] >= 0 && numbers[i] <= 9)
+//    {
+//        Console.WriteLine(numbers[i]);
+//        reqemSayi = reqemSayi + 1;
+//    }
+
+//}
+//Console.WriteLine(reqemSayi);
+#endregion
+
+
+#region
+//2.Verilmiş ədədin neçə mərtəbəli olduğunu print edən alqoritmin diaqramını qurun.
+//(məsələn: 321 ədədi 3 mərtəbəlidir, 23125 ədədi 5 mərtəbəlidir.)
+
+//int number = 12332145;
+//int mertebeSayi = 0;
+//while (number > 0)
+//{
+//    int digit = number % 10;
+//    number = number / 10;
+//    mertebeSayi = mertebeSayi + 1;
+
+//}
+//Console.WriteLine(mertebeSayi);
+#endregion
+
+
+//#region
+////1..İki ədəd verilir. Birinci ədədin ikinci ədəd qüvvətini hesablayan alqoritmin diaqramını qurun.
+////(məsələn: k və g verilir kᵍ print olunmalıdır.) ( loop ilə həll olunmalıdır.)
+//int num1 = 2;
+//int num2 = 5;
+//int pow = 1;
+//for(int i = 1; i <= num2;i++)
+//{
+    
+//    pow = num1 * pow;
+//}
+//Console.WriteLine(pow);
+
+//#endregion
